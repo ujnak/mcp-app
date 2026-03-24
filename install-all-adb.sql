0fc406ebf755f3fdaeba2b830070fe69178342b9
Yuji N <yuji.nkks@gmail.com>
2026-03-24 20:01:43 +0900

    add comment for PLSQL_CCFLAGS

--- install-all-adb.sql
+++ install-all-adb.sql
@@ -1,2 +1,5 @@
+-- Although is_autonomous in PLSQL_CCFLAGS is configured via ALTER SESSION,
+-- it is also necessary to configure it using ALTER SYSTEM,
+-- considering that packages may be compiled automatically.
 alter session set PLSQL_CCFLAGS = 'is_autonomous:TRUE';
 @@install-all.sql
